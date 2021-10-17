d7eaf39eb9aea9ca802539157db3b9353287ee17
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-10-17 14:56:54 +0100

    url setup completed

---
+++ blogAPI/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import PostList, PostDetail
+
+app_name = 'blogAPI'
+
+urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name="ViewPost"),  #shows individual post in database
+    path('', PostList.as_view(), name="AllPosts"),  #shows the list of all posts in database
+]
--- blogsite/urls.py
+++ blogsite/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
+    path('api/', include('blogAPI.urls', namespace='blog_api')),
 ]
